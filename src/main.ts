1eebdda3259a4a5d16fabee24d37c596c68d131a
Kolotylo Kyrylo <kolot102@gmail.com>
2024-06-12 12:41:01 +0300

    preflight

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  app.enableCors({
+
+  const corsOptions: CorsOptions = {
     origin: ['https://diploma-rust.vercel.app/'],
     credentials: true,
     allowedHeaders: 'Content-Type, Accept',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     exposedHeaders: 'set-cookie',
-  });
+    preflightContinue: true,
+  };
+
+  app.enableCors(corsOptions);
 
   await app.listen(process.env.PORT, '0.0.0.0');
 }
